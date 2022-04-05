894907ad0e09071dd29fb372239ef8f89c74badf
dasbente <dasbente@gmail.com>
2022-04-05 21:29:36 +0200

    Build popup from svelte

--- extension/popup.js
+++
@@ -1,5 +0,0 @@
-setInterval(function () {
-    browser.tabs.query({ currentWindow: true, active: true }).then(function (tabs) {
-        document.getElementById("text").innerText = tabs[0].url;
-    })
-}, 1000)
