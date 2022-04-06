2714e568a521c32fb9762df77aa0a78567857602
dasbente <dasbente@gmail.com>
2022-04-06 22:47:50 +0200

    Trim down content_scripts In the future this will not generate any HTML so we can remove all svelte code from this part of the application.

--- content_scripts/rollup.config.js
+++ content_scripts/rollup.config.js
@@ -1,32 +1,8 @@
-import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
-
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
 
 export default {
 	input: 'src/main.js',
@@ -37,16 +13,6 @@ export default {
 		file: '../extension/build/content/bundle.js'
 	},
 	plugins: [
-		svelte({
-			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production
-			}
-		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
-
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
@@ -57,14 +23,6 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
--- content_scripts/src/main.js
+++
@@ -1,8 +0,0 @@
-import Extension from './Extension.svelte';
-
-const node = document.createElement('div');
-document.body.appendChild(node);
-
-const app = new Extension({ target: node, props: {} });
-
-export default app;
