a677b7054138ca8b1cbeee54e721ed5bbf4a00f4
dasbente <dasbente@gmail.com>
2022-04-08 14:21:36 +0200

    Add usage hint

--- popup/src/App.svelte
+++ popup/src/App.svelte
@@ -1,11 +1,22 @@
 <script>
     import Table from './table/index.svelte';
-
+    
     const fetch = async () => await browser.runtime.sendMessage({ id: 'getScore' });
 </script>
 
+<style>p { width: 200px; }</style>
+
 <main>
     {#await fetch() then data}
-        <Table data={data.results} />
+        {#if data.results}
+            <Table data={data.results} />
+        {:else}
+            <h4>No data</h4>
+            <p>
+                Please visit the 
+                <a href="https://www.youtube.com/paid_memberships">YouTube Payment Page</a>
+                 to allow simp-io to collect the necessary data for operation.
+            </p>
+        {/if}
     {/await}
 </main>
