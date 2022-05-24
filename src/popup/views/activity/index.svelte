c39510c659771e67fa7792076dfa3465b27b9617
dasbente <dasbente@gmail.com>
2022-05-24 20:37:36 +0200

    Add new page for activity data
    Activities are all superchats (in relevant metrics)
    that were bought at a particular date.

---
+++ src/popup/views/activity/index.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {stats} from '../../stores/payments';
+</script>
+
+<div>
+    <svg>
+
+    </svg>
+</div>
+
+<footer>
+    <!-- TODO: Implement controls -->
+</footer>
+
+<style>
+    div {
+        height: 510px;
+        width: 100%;
+        padding: 4px;
+    }
+
+    footer {
+        width: 100%;
+        height: 50px;
+        color: white;
+        background: black;
+    }
+</style>
---
+++ src/popup/views/pages.js
@@ -0,0 +1,7 @@
+import Table from './table/index.svelte';
+import Activity from './activity/index.svelte';
+
+export default {
+    'table': {title: 'Table', component: Table},
+    'activity': {title: 'Activity', component: Activity}
+}
