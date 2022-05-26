ca482667732728f05efbec2b7c0e0ce5879d3520
dasbente <dasbente@gmail.com>
2022-05-26 23:36:20 +0200

    Clean up a bit and add past year view

--- src/popup/views/activity/Range.svelte
+++ src/popup/views/activity/Range.svelte
@@ -1,46 +1,51 @@
 <script>
-    import {scaleBand, scaleLinear} from 'd3-scale';
-    import {dateRange} from "./processing";
+    import {scaleBand, scaleLog, scaleSequential} from 'd3-scale';
+    import {interpolatePlasma} from 'd3-scale-chromatic';
+    import {datesForVisualization} from "./processing";
 
-    export let year, processing;
-    let range = dateRange(year || 2000);
+    export let processing;
+
+    let range = datesForVisualization();
+    let values = range.map(r => processing(r.date));
+
+    console.log(range);
 
     let monthPad = 3;
 
     let xScale = scaleBand()
         .domain([...Array(27).keys()])
-        .range([30, 800 - monthPad * 12])
+        .range([30, 800 - monthPad * 13])
         .paddingInner(.05).paddingOuter(.1);
 
     let weekdays = "SMTWTFS".repeat(2).split("");
     let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    let values = range.map(r => processing(r.date));
-    console.log(values.filter(v => v > 0));
+    let valueScale = scaleLog().domain([1, 500]).range([0,1]);
+    let colorScale = scaleSequential().domain([0, 1]).interpolator(interpolatePlasma);
 
-    let colorRange = scaleLinear().range(['#eeeeee', '#FCD748', '#F6ADAF', '#770077'])
-        .domain([0, 10, 100, 500]);
+    let scaleValue = val => val === 0 ? '#dddddd' : colorScale(valueScale(val));
 </script>
 
-<!-- Weekday labels -->
+<!-- Weekday labels-->
 {#each weekdays as w, i}
     <text x={xScale(0) - 20} y={xScale(i) + xScale.bandwidth() / 2} class:sunday={i % 7 === 0} dominant-baseline="central">
         {w}
     </text>
 {/each}
 
-{#each range as {weekday, week, month, day}, i}
+{#each range as {weekday, week, month, day, yearOff, monthOff}, i}
+    <!-- Month labels -->
     {#if day === 1}
-        <text x={xScale(Math.floor(week / 2)) + month * monthPad} y={xScale(14) + 5} dominant-baseline="hanging">
+        <text x={xScale(week) + month * monthPad} y={xScale(14) + 5} dominant-baseline="hanging">
             {months[month]}
         </text>
     {/if}
 
-    <rect x={xScale(Math.floor(week / 2)) + month * monthPad}
-          y={xScale(weekday + ((week % 2) * 7)) + (day === 1 && weekday !== 0 && week % 2 === 1 ? monthPad : 0)}
-          fill={colorRange(values[i])}
+    <rect x={xScale(week) + monthOff * monthPad + yearOff * monthPad}
+          y={xScale(weekday) + (weekday !== 0 ? monthPad : 0)}
+          fill={scaleValue(values[i])}
           width={xScale.bandwidth()}
-          height={xScale.bandwidth() - (day === 1 && weekday !== 0 && week % 2 === 1 ? monthPad : 0)}></rect>
+          height={xScale.bandwidth() - (weekday !== 0 ? monthPad : 0)}></rect>
 {/each}
 
 
--- src/popup/views/activity/processing.js
+++ src/popup/views/activity/processing.js
@@ -1,30 +1,40 @@
 import moment from 'moment';
 
-export const dateRange = year => {
-    let start = moment(`${year}-01-01`);
-    let end = moment(`${year}-12-31`);
-
-    let week = 0;
-    let weekCounter = start.weekday()
-
+/**
+ * Generates a range of dates between the two given dates. (Inclusive)
+ * @param start First day of the range
+ * @param end Last day of the range
+ * @returns {*[Date]} Array of dates
+ */
+export const dateRange = (start, end) => {
     let range = [];
-    while (start.isSameOrBefore(end)) {
-        range.push({
-            date: start.format('YYYY-MM-DD'),
-            month: start.month(),
-            weekday: start.weekday(),
-            day: start.date(),
-            week
-        });
 
-        start = start.add(1, 'd')
-
-        weekCounter++;
-        if (weekCounter === 7) {
-            weekCounter = 0;
-            week++;
-        }
+    start = moment(start);
+    while (start.isSameOrBefore(end, 'day')) {
+        range.push(start.toDate());
+        start = start.add(1, 'd');
     }
 
-    return range
+    return range;
+}
+
+export const datesForVisualization = (year = false, weekStack = 2) => {
+    let end = year ? moment(`${year}-12-31`) : moment();
+    let start = moment(end).add(-1, 'year');
+
+    console.log(end);
+    console.log(start);
+
+    return dateRange(start, end).map((d, i) => {
+        let day = moment(d);
+
+        return {
+            day: day.toDate(), month: day.month(), year: day.year(),
+            week: Math.floor((i + start.weekday()) / (7 * weekStack)),
+            weekday: (i + start.weekday()) % (7 * weekStack),
+            date: day.format('YYYY-MM-DD'),
+            yearOff: day.year() - start.year(),
+            monthOff: (day.year() - start.year()) * 12 + day.month() - start.month()
+        };
+    });
 }
