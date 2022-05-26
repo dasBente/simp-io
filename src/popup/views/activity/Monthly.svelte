b2e3e25d9b51c2b8977840063f9205e963f6e5e7
dasbente <dasbente@gmail.com>
2022-05-26 09:13:27 +0200

    Potential look and feel for the activity view

---
+++ src/popup/views/activity/Monthly.svelte
@@ -0,0 +1,17 @@
+<script>
+    import moment from "moment";
+
+    export let y, scale, year, month;
+
+    let size = scale.bandwidth();
+
+    // normalize date format
+    let norm = moment(`${year}-${month}`, 'YYYY-MMM').format('YYYY-MM');
+
+    let first = moment(`${norm}-01`).day();
+    let last = moment(norm).daysInMonth();
+</script>
+
+{#each [...Array(37).keys()].slice(first, first + last) as i, j}
+    <rect x="{scale(i)}" {y} width={size} height={size} fill="green"></rect>
+{/each}
--- src/popup/views/activity/index.svelte
+++ src/popup/views/activity/index.svelte
@@ -1,29 +1,51 @@
 <script>
     import {years} from '../../stores/payments';
-    console.log($years);
+    import Monthly from "./Monthly.svelte";
+
+    import {scaleBand} from "d3-scale";
+
+    let dayScale = scaleBand()
+        .domain([...Array(37).keys()]).range([40, 796])
+        .paddingInner(.05);
+
+    let size = dayScale.bandwidth();
+
+    let weekdays = "SMTWTFS".repeat(6).slice(0, 37).split("");
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    let year = $years[0];
 </script>
 
 <div>
     <svg>
+        <!-- Label weekdays -->
+        {#each weekdays as w, i}
+            <text x={dayScale(i) + dayScale.bandwidth() / 2} y={dayScale(0) - 8} text-anchor="middle" class:sunday={i % 7 === 0}>
+                {w}
+            </text>
+        {/each}
 
+        <!-- Label Months -->
+        {#each months as m, i}
+            <text x={dayScale(0) - 35} y={dayScale(i) + dayScale.bandwidth() / 2} dominant-baseline="central">
+                {m}
+            </text>
+        {/each}
+
+        {#each months as month, j}
+            <Monthly y={dayScale(j)} scale={dayScale} {year} {month} />
+        {/each}
     </svg>
 </div>
-
-<footer>
-    <!-- TODO: Implement controls -->
-</footer>
 
 <style>
     div {
         height: 510px;
         width: 100%;
-        padding: 4px;
     }
 
-    footer {
-        width: 100%;
-        height: 50px;
-        color: white;
-        background: black;
-    }
+    svg { width: 100%; height: 100%; }
+
+    .sunday { fill: orangered; font-weight: bolder; }
+
 </style>
