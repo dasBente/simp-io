a9ce3c2e860ae09c766fd28409378856f851561b
dasbente <dasbente@gmail.com>
2022-09-12 13:54:28 +0200

    Move to typescript (rough)

--- rollup.config.js
+++ rollup.config.js
@@ -2,43 +2,38 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
+const makePlugins = (opts = {svelte: false, css: undefined}) => [
+	opts.svelte && svelte({
+		preprocess: sveltePreprocess({sourceMap: !production}),
+		compilerOptions: {
+			// enable run-time checks when not in production
+			dev: !production
+		}
+	}),
+	opts.css && css({output: `${opts.css}-bundle.css`}),
+	resolve(opts.svelte ? {browser: true, dedupe: ['svelte']} : {browser: true}),
+	commonjs(),
+	typescript({sourceMap: !production, inlineSources: !production}),
+	production && terser()
+];
+
 export default [
 	/** Pop-up **/
 	{
-		input: 'src/popup/main.js',
+		input: 'src/popup/main.ts',
 		output: {
 			sourcemap: true,
 			format: 'iife',
 			name: 'popup',
 			file: './extension_template/default/build/popup-bundle.js'
 		},
-		plugins: [
-			svelte({
-				compilerOptions: {
-					// enable run-time checks when not in production
-					dev: !production
-				}
-			}),
-
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css({ output: 'popup-bundle.css' }),
-
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-
-			commonjs(),
-
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			terser()
-		],
+		plugins: makePlugins({svelte: true, css: 'popup'}),
 		watch: {
 			clearScreen: false
 		}
@@ -46,18 +41,14 @@ export default [
 
 	/** Background Script **/
 	{
-		input: 'src/background/main.js',
+		input: 'src/background/main.ts',
 		output: {
 			sourcemap: true,
 			format: 'iife',
 			name: 'background',
 			file: './extension_template/default/build/background.js'
 		},
-		plugins: [
-			resolve({ browser: true }),
-			commonjs(),
-			terser()
-		],
+		plugins: makePlugins(),
 		watch: {
 			clearScreen: false
 		}
@@ -65,18 +56,14 @@ export default [
 
 	/** Content Script **/
 	{
-		input: 'src/content/main.js',
+		input: 'src/content/main.ts',
 		output: {
 			sourcemap: true,
 			format: 'iife',
 			name: 'content',
 			file: './extension_template/default/build/content.js'
 		},
-		plugins: [
-			resolve({ browser: true }),
-			commonjs(),
-			terser()
-		],
+		plugins: makePlugins(),
 		watch: {
 			clearScreen: false
 		}
